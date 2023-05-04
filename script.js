cdadf4723741b75b6ba8ab0f5fc9c5529d518a13
Əmiraslan E. Məmmədov <amiraslan.mammadov@cybernet.az>
2023-05-04 09:06:30 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var number1 = +prompt();
+var number2 = +prompt();
+var number3 = +prompt();
+var number4 = +prompt();
+var number5 = +prompt();
+
+alert(number1 * number2* number3* number4* number5);
